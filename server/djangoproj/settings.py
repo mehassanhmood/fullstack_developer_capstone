8755c92ee7478659b7f6fc1ddb92be44fb7deec6
mehassanhmood <mehassanhmood@gmail.com>
2024-05-20 02:39:57 -0400

    Django Proxy Services

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1','http://127.0.0.1:8000','localhost','mehassanhmoo-8000']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
 REST_FRAMEWORK = {
