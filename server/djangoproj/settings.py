7d68305d97749fccdd62296ee7a9ff2357e30ced
Hassan Mehmood <mehassanhmood@gmail.com>
2024-05-25 19:50:36 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','http://127.0.0.1:8000','localhost','mehassanhmoo-8000']
+ALLOWED_HOSTS = ['127.0.0.1', 'http://127.0.0.1:8000', 'localhost', 'mehassanhmoo-8000']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
 REST_FRAMEWORK = {
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
