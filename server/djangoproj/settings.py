1081b9cecdebc81eea6c1fa51b00623bdf53bac7
Hassan Mehmood <mehassanhmood@gmail.com>
2024-05-25 19:53:42 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://127.0.0.1:8000', 'localhost', 'mehassanhmoo-8000']
+ALLOWED_HOSTS = ['127.0.0.1', 'http://127.0.0.1:8000',
+                 'localhost', 'mehassanhmoo-8000']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
 REST_FRAMEWORK = {
@@ -139,8 +140,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
