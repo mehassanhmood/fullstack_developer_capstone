50fa6fb4a6c3b3204e9795858c6af8c011b3b751
Hassan Mehmood <mehassanhmood@gmail.com>
2024-05-25 20:13:41 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,7 +1,5 @@
 # Uncomment the required imports before adding the code
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
@@ -137,7 +135,7 @@ def get_dealer_reviews(request, dealer_id):
 # ...
 @csrf_exempt
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if request.user.is_anonymous == False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
